f90feedb31ffa2b654bbd83c7cfe56e94e693f68
karol <karol@karol-MS-7623.(none)>
2017-03-12 12:41:05 +0100

    Fixed getting products from date. Removed 'valueable' field from grid

--- ddiet/daytable.py
+++ ddiet/daytable.py
@@ -11,7 +11,7 @@ class DayTable():
         self.date = date
 
     def get_products(self):
-        products = models.ProductData.objects.filter(eat_date__gte=self.date).values('product', 'amount')
+        products = models.ProductData.objects.filter(eat_date=self.date).values('product', 'amount')
         js_products = []
         for row in products:
             product = models.Product.objects.get(pk=row['product'])
@@ -21,8 +21,7 @@ class DayTable():
                      'Carbs'   :  str(product.carbs * amount / 100.0),
                      'Fat'     :  str(product.fat * amount / 100.0),
                      'Protein' :  str(product.protein * amount / 100.0),
-                     'Kcal'    :  str((amount / 100.0) * (4 * product.carbs + 9 * product.fat + 4 * product.protein)),
-                     'Valuable':  "true"}
+                     'Kcal'    :  str((amount / 100.0) * (4 * product.carbs + 9 * product.fat + 4 * product.protein))}
             js_products.append(js_pr)
         #todo sum in different structure, use queries instead of manual sum
         js_sum = {'Product' : 'Sum',
@@ -30,8 +29,7 @@ class DayTable():
                   'Carbs'   : str(sum(float(p['Carbs']) for p in js_products)),
                   'Fat'     : str(sum(float(p['Fat']) for p in js_products)),
                   'Protein' : str(sum(float(p['Protein']) for p in js_products)),
-                  'Kcal'    : str(sum(int(float(p['Kcal'])) for p in js_products)),
-                  'Valuable':  "false"}
+                  'Kcal'    : str(sum(int(float(p['Kcal'])) for p in js_products))}
         js_products.append(js_sum)
         return json.dumps(js_products, cls=DjangoJSONEncoder)
 
--- ddiet/products.py
+++ ddiet/products.py
@@ -9,10 +9,10 @@ class Products:
         try:
             product = models.Product.objects.get(name=prodName)
             coeff = float(amount) / 100.0
-            rv = { "Carbs"   : str(product.carbs * coeff),
-                   "Fat"     : product.fat * coeff,
-                   "Protein" : str(product.protein * coeff),
-                   "Kcal"    : str(coeff * (4 * product.carbs + 9 * product.fat + 4 * product.protein)) }
+            rv = { "Carbs"   : '%.1f' % (product.carbs * coeff),
+                   "Fat"     : '%.1f' % (product.fat * coeff),
+                   "Protein" : '%.1f' % (product.protein * coeff),
+                   "Kcal"    : '%.1f' % (coeff * (4 * product.carbs + 9 * product.fat + 4 * product.protein)) }
             return json.dumps(rv, cls=DjangoJSONEncoder)
         except models.Product.DoesNotExist:
             return None
